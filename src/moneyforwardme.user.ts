9e3f53edbf09d287ea6ebd563055f33896cf17a4
aozou99 <hellostacysmom@gmail.com>
2024-05-23 23:18:00 +0900

    remove unused user-script-headers

--- src/moneyforwardme.user.ts
+++ src/moneyforwardme.user.ts
@@ -1,18 +1,3 @@
-// ==UserScript==
-// @name マネーフォワードME Web
-// @namespace https://github.com/aozou99/TempermonkeyScripts
-// @version v0.1.1
-// @description Make a few changes to the design of MoneyforwardME
-// @author A.A
-// @match https://moneyforward.com/bs/portfolio
-// @match https://moneyforward.com/bs/history
-// @iconURL https://assets.moneyforward.com/assets/favicon-710b014dd04a85070bb0a55fa894b599015b5310333d38da9c85ad03594bbc20.ico
-// @grant none
-// @updateURL https://github.com/aozou99/TempermonkeyScripts/raw/main/dist/moneyforward.user.js
-// @downloadURL https://github.com/aozou99/TempermonkeyScripts/raw/main/dist/moneyforward.user.js
-// @supportURL https://github.com/aozou99/TempermonkeyScripts
-// ==/UserScript==
-
 import { handlePortfolioPage } from './pages/portfolio';
 import { handleHistoryPage } from './pages/history';
 
