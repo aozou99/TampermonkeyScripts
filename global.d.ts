d455f46f7ad6f11d8d39d34070f060430acf5399
aozou99 <hellostacysmom@gmail.com>
2024-05-28 10:38:04 +0900

    fix global define

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,4 @@
-export const labels = ['預金・現金・暗号資産', '投資信託', '株式', 'ポイント'] as const;
+const labels = ['預金・現金・暗号資産', '投資信託', '年金', '株式', 'ポイント'] as const;
 export type Labels = (typeof labels)[number];
 
 export interface TimeSeriesData {
